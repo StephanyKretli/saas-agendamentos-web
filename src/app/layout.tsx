3cc5023c1d8d476724f65762552951f4d0637343
Stephany Kretli <stephanykretli@gmail.com>
2026-03-09 20:49:26 -0300

    feat: initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import { QueryProvider } from "@/components/providers/query-provider";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" className={cn("font-sans", geist.variable)}>
+      <body>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
